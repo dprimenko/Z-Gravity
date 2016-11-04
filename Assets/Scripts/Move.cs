1fc79324eff0a699bfd0c3ba613b1410dc0a7ddd
David <davidprimenko@gmail.com>
2016-11-04 13:41:10 +0100

    Initial commit

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		//GetComponent<Rigidbody2D> ().AddForce ((Vector2.right * 3f));
+		GetComponent<Rigidbody2D> ().AddForce (new Vector2((Input.acceleration.x * 5f), 0));
+
+		foreach (Touch touch in Input.touches) {
+				GetComponent<Rigidbody2D> ().AddForce (Vector2.up, ForceMode2D.Impulse );
+		}
+	}
+}
